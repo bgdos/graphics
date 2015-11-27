e10fa4a2a92427b6fb235469fee3f53a6db9848e
Juan Salgado <juansutt@hotmail.com>
2015-11-27 15:03:33 -0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,88 +1,86 @@
-var int;
-var minimo;
-var maximo;
-var intervalo;
-var titulosX = ['Mon','Tue','Thu','Wed','Fri']
-//function columns(element, datos, titulosY, titulosX)
-function makeGraphics()
+var titulosX = ['Mon','Tue','Thu','Wed','Fri', 'Sat', 'Sun', 'Lun', 'Mar', 'Mier', 'Jue', 'Vie', 'Sab', 'Dom']
+/** Menu para la seleccion de tipo de  grafica **/
+function displayGraphics()
+{
+    var c = makeChart();//crear grafica con los datos de la pagina web
+    var option = parseInt(getElement('chartType').value);
+    switch(option) {//desplegar el tipo de grafica seleccionada
+        case 0: popInfo('Error','Select a chart type');
+            break;
+        case 1: c.bar();
+            break;
+        case 2: c.columns();
+            break;
+        case 3:
+            c.labelY = getElement('titleY').value;//se agrega el titulo al eje Y
+            c.labelX = getElement('titleX').value;//se agrega el titulo al eje X
+            c.linear();
+            break;
+        case 4: c.pie();
+            break;
+        case 5:
+            c.labelY = getElement('titleY').value;//se agrega el titulo al eje Y
+            c.labelX = getElement('titleX').value;//se agrega el titulo al eje X
+            c.average = getElement('average').value;//se agrega un valor promedio preestablecido
+            c.points();
+            break;
+        default: alert('error', 'Select a chart type');
+            break;
+    }
+}
+function makeChart()
 {
     var array = [];
-    for (var i = 1; i <= 5; i++)
+    var inputs = getElement('values').querySelectorAll('input');
+    for (var i = 1; i <= inputs.length; i++)
     {
-        var n = parseFloat(document.getElementById('value' + i).value);
+        var n = parseFloat(getElement('value' + i).value);
         if (!isNaN(n))
             array.push(n);
     }
     if (array.length > 0)
     {
-        var textarea = document.getElementById('datasheet');
-        textarea.innerHTML = '';
-        var max = Math.max.apply(Math, array);
-        textarea.value = 'Maximum: ' + max + '\n';
-        var min = Math.min.apply(Math, array);
-        textarea.value += 'Minimum: ' + min + '\n';
-        //multiplicador
-        var multi = .00001;
-        //encontrar el maximo del multiplicador
-        while (max > multi)
-        {
-            multi = multi * 10;
-        }
-        //determinar el maximo
-        var maximum = 0;
-        var rounded = Math.round(max / 10);
-        if (max < 1)
-            maximum = multi;
-        else if (max <= 100 & min >= 1)
-            maximum = (rounded > max / 10) ? rounded * 10 : (rounded + 1) * 10;
-        else while (maximum <= max)
-            maximum += (multi / 100);
-        //determinar el minimo
-        var zero = document.getElementById('zero');
-        var minimum = 0;
-        if (min <= multi & min <= 100 || zero.checked)
-            minimum = 0;
-        else while (min > minimum + multi / 100)
-                    minimum += multi / 100;
-        var interval = (maximum - minimum) / 10;
-        //desplegar resultados para grafica en consola
-        textarea.value += 'Multiplicator : ' + multi + '\n';
-        textarea.value += 'Calculated Maximum : ' + maximum + '\n';
-        textarea.value += 'Calculated Minimum : ' + minimum + '\n';
-        textarea.value += 'Calculated Interval: ' + interval + '\n';
-        //llenar los intervalos
-        var intervals = []
-        for (var i = 1; i <= 10; i++)
-            intervals.push(i * interval + minimum);
-        columns('columns', array, intervals, titulosX, minimum, maximum);
+        var z = getElement('zero').checked;
+        var chart = new Chart('columns', array, titulosX, z);
+        return chart;
     }
 }
 function exportToImage()
 {
-    var svg = document.getElementById("columns");
-    var canvas = document.getElementById('canvas');
-    canvg(canvas, unescape(svg.outerHTML));  
+    var svg = getElement("columns").outerHTML;
+    //ssvg = unescape(svg);
+    var canvas = getElement('canvas');
+    canvg(canvas, svg);  
     var theImage = canvas.toDataURL('image/png');
-    /*document.write('<img src="'+theImage+'"/>');
+    var img = document.createElement('img');
+    img.getAttribute('src', theImage);
+    //document.write('<img src="'+theImage+'"/>');
     var a = document.createElement('a');
     a.href     = theImage;
     a.download = 'chart';
     a.target   = '_blank';
     document.body.appendChild(a); 
     a.click(); 
-    document.body.removeChild(a);*/
+    document.body.removeChild(a);
 }
-function enterButton(element)
+function checkOptions(value)
 {
-    document.onkeypress = keyPress;
-
-        function keyPress(e)
-        {
-            var x = e || window.event;
-            var key = (x.keyCode || x.which);
-             if(key == 13 || key == 3){
-             document.getElementById(element).focus();
-			document.getElementById(element).onclick();
-        }
-    }
+    if (value == 5 || value == 3)
+        getElement('titleY').style.display = getElement('titleX').style.display = 'inline';
+    else getElement('titleY').style.display = getElement('titleX').style.display = 'none';
+    if (value == 5)
+        getElement('average').style.display = 'inline';
+    else getElement('average').style.display = 'none';
+    
+}
+var contador = 7;
+function createInput()
+{
+    var input = document.createElement('input');
+    input.setAttribute("id", "value" + contador);
+    input.setAttribute("type", 'number');
+    input.setAttribute("placeholder", 'value');
+    getElement('values').appendChild(input);
+    contador ++;
+    varColors.push(getRandomColor());
 }
